e87cee797d07cec3e20540a866aacbaf8b08d221
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-08-27 20:28:00 +0900

    cuda index header

--- src/matrix_add/src/main.cu
+++ src/matrix_add/src/main.cu
@@ -6,27 +6,7 @@
 #include <cstdlib>
 #include <string>
 
-#define TID_X threadIdx.x
-#define TID_Y threadIdx.y
-#define TID_Z threadIdx.z
-
-#define BID_X blockIdx.x
-#define BID_Y blockIdx.y
-#define BID_Z blockIdx.z
-
-#define GDIM_X gridDim.x
-#define GDIM_Y gridDim.y
-#define GDIM_Z gridDim.z
-
-#define BDIM_X blockDim.x
-#define BDIM_Y blockDim.y
-#define BDIM_Z blockDim.z
-
-#define TID_IN_BLOCK (TID_Z * (BDIM_Y * BDIM_X) + TID_Y * BDIM_X + TID_X)
-#define NUM_THREADS_IN_BLOCK (BDIM_X * BDIM_Y * BDIM_Z)
-
-#define BLOCK_ID_LINEAR (BID_X + GDIM_X * (BID_Y + GDIM_Y * BID_Z))
-#define GLOBAL_TID (TID_IN_BLOCK + (BLOCK_ID_LINEAR * NUM_THREADS_IN_BLOCK))
+#include "index.cuh"
 
 #define ROW 32
 #define COL 32
@@ -50,7 +30,7 @@ __global__ void matrix_add(const float *a, const float *b, float *c, int length)
 
   if (i < length) {
     c[i] = a[i] + b[i] + 0.0F;
-    // printf("GID %4u=%4d (%2d, %2d) | %f = %f + %f\n", GLOBAL_TID, i, row, col, c[i], a[i], b[i]);
+    printf("GID %4u=%4d (%2d, %2d) | %f = %f + %f\n", GLOBAL_TID, i, row, col, c[i], a[i], b[i]);
   }
 }
 
